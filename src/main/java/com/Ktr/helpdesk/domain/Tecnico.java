ab6da043d2daf379f42f1c5f84050827e709d245
Romenildo <Romenildoferreira@gmail.com>
2022-06-15 09:51:18 -0300

    ligacao com banco de dados h2

--- src/main/java/com/Ktr/helpdesk/domain/Tecnico.java
+++ src/main/java/com/Ktr/helpdesk/domain/Tecnico.java
@@ -6,6 +6,8 @@ import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
+import com.Ktr.helpdesk.domain.enums.Perfil;
+
 @Entity
 public class Tecnico extends Pessoa{
     private static final long serialVersionUID = 1L;
@@ -15,10 +17,12 @@ public class Tecnico extends Pessoa{
 
     public Tecnico() {
         super();
+        addPerfil(Perfil.TECNICO);
     }
 
     public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
         super(id, nome, cpf, email, senha);
+        addPerfil(Perfil.TECNICO);
     }
 
     public List<Chamado> getChamados() {
