a11e58a83c0d54a00de67f2b1c980b3aa390223a
Romenildo <Romenildoferreira@gmail.com>
2022-06-16 18:56:11 -0300

    authorization

--- src/main/java/com/Ktr/helpdesk/controllers/TecnicoController.java
+++ src/main/java/com/Ktr/helpdesk/controllers/TecnicoController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -52,6 +53,8 @@ public class TecnicoController {
         return ResponseEntity.ok().body(listDTO);
     }
 
+    //adicionar a autorizacao necessaria para a requisicao
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @PostMapping
     public ResponseEntity<TecnicoDTO> create(@Valid @RequestBody TecnicoDTO objDTO){
         Tecnico newObj = service.create(objDTO);
@@ -61,6 +64,8 @@ public class TecnicoController {
         return ResponseEntity.created(uri).build();
     }
 
+    //adicionar a autorizacao necessaria para a requisicao
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @PutMapping(value = "/{id}")
     public ResponseEntity<TecnicoDTO> update(@PathVariable Integer id,@Valid @RequestBody TecnicoDTO objDTO){
 
@@ -68,6 +73,8 @@ public class TecnicoController {
         return ResponseEntity.ok().body(new TecnicoDTO(obj));
     }
 
+    //adicionar a autorizacao necessaria para a requisicao
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<TecnicoDTO> delete(@PathVariable Integer id){
         service.delete(id);
--- src/main/java/com/Ktr/helpdesk/security/JWTUtil.java
+++ src/main/java/com/Ktr/helpdesk/security/JWTUtil.java
@@ -5,6 +5,8 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwt;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
@@ -28,5 +30,36 @@ public class JWTUtil {
                     .compact();
     }
     //final gerar o filtro de autorizacao
+
+    public boolean tokenValido(String token) {
+        Claims claims = getClaims(token);
+        if(claims != null){
+            String username = claims.getSubject();
+            Date expirationDate = claims.getExpiration();
+            Date now = new Date(System.currentTimeMillis());
+
+            if(username != null && expirationDate != null && now.before(expirationDate)){
+                return true;
+            }   
+        }
+        return false;
+    }
+
+    private Claims getClaims(String token) {
+        try {
+            return Jwts.parser().setSigningKey(secret.getBytes()).parseClaimsJws(token).getBody();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    //A partir do token tirar o username do claims
+    public String getUsername(String token) {
+        Claims claims = getClaims(token);
+        if(claims != null){
+            return claims.getSubject();
+        }
+        return null;
+    }
 
 }
