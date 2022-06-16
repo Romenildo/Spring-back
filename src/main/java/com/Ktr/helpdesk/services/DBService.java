f42e84ebe7eb76577059761fa8a232f381306842
Romenildo <Romenildoferreira@gmail.com>
2022-06-16 20:11:30 -0300

    finalizacao e separacao de perfis entre test(h2) e dev(sql)

--- src/main/java/com/Ktr/helpdesk/services/DBService.java
+++ src/main/java/com/Ktr/helpdesk/services/DBService.java
@@ -6,7 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.Ktr.helpdesk.domain.Chamado;
+import com.Ktr.helpdesk.domain.Cliente;
 import com.Ktr.helpdesk.domain.Tecnico;
+import com.Ktr.helpdesk.domain.enums.Perfil;
+import com.Ktr.helpdesk.domain.enums.Prioridade;
+import com.Ktr.helpdesk.domain.enums.Status;
 import com.Ktr.helpdesk.repositories.ChamadoRepository;
 import com.Ktr.helpdesk.repositories.PessoaRepository;
 
@@ -23,7 +28,13 @@ public class DBService {
     public void instanciaDB(){
 
         Tecnico tec1 = new Tecnico(null, "Rom", "123.234.123-11","rom@gmail.com",encoder.encode("123"));
+        tec1.addPerfil(Perfil.ADMIN);
 
-        pessoaRepository.saveAll(Arrays.asList(tec1));
+        Cliente cli1 = new Cliente(null, "joao", "1821921831911", "joao@gmail.com", encoder.encode("12345"));
+
+        Chamado c1 = new Chamado(null, Prioridade.MEDIA,Status.ANDAMENTO, "Chamado 01","Primeiro Chamado",tec1, cli1);
+
+        pessoaRepository.saveAll(Arrays.asList(tec1, cli1));
+        chamadoRepository.saveAll(Arrays.asList(c1));
     }
 }
